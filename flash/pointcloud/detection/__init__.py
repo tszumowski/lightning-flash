ed81f590c378308c1384bea3473d3bea3e214898
Ethan Harris <ethanwharris@gmail.com>
2021-07-16 23:22:43 +0100

    Try fix

--- flash/pointcloud/detection/__init__.py
+++ flash/pointcloud/detection/__init__.py
@@ -1,3 +1,4 @@
+from flash.pointcloud.detection import open3d_ml  # noqa: F401
 from flash.pointcloud.detection.data import PointCloudObjectDetectorData  # noqa: F401
 from flash.pointcloud.detection.model import PointCloudObjectDetector  # noqa: F401
 from flash.pointcloud.detection.open3d_ml.app import launch_app  # noqa: F401
