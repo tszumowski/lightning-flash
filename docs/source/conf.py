c2bb079ed36db9767241f7e8bb3c0b7e26a47798
Ethan Harris <ethanwharris@gmail.com>
2021-07-16 22:23:39 +0100

    Try fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -156,7 +156,7 @@ PACKAGE_MAPPING = {
     'pytorch-lightning': 'pytorch_lightning',
     'scikit-learn': 'sklearn',
     'Pillow': 'PIL',
-    'PyYAML': 'yaml',
+    'PyYAML>=5.1': 'yaml',
     'rouge-score': 'rouge_score',
     'lightning-bolts': 'pl_bolts',
     'pytorch-tabnet': 'pytorch_tabnet',
